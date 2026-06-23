4d45c6e2b1e5c52b64e64956761e5fa239a2c1f2
R Bhuvan Dhanush <bhuvandhanush2006@gmail.com>
2026-06-23 14:20:06 +0530

    Add Calculator class with add and subtract methods

--- Week-1/Junit testing/ Setting Up JUnit/src/main/java/com/bank/app/Calculator.java
+++ Week-1/Junit testing/ Setting Up JUnit/src/main/java/com/bank/app/Calculator.java
@@ -1,1 +1,14 @@
+package com.bank.app;
 
+public class Calculator {
+
+    // Simple addition logic to target with unit tests
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Simple deduction logic to target with unit tests
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
