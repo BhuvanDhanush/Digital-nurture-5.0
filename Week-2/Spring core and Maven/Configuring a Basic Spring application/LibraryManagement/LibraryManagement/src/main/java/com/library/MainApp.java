be49b4d48b362c8bd58c01d19e371273d1b16a92
R Bhuvan Dhanush <bhuvandhanush2006@gmail.com>
2026-06-30 08:59:01 +0530

    Clean up comments in MainApp.java
    Removed comments from the main application class to clean up the code.

--- Week-2/Spring core and Maven/Configuring a Basic Spring application/LibraryManagement/LibraryManagement/src/main/java/com/library/MainApp.java
+++ Week-2/Spring core and Maven/Configuring a Basic Spring application/LibraryManagement/LibraryManagement/src/main/java/com/library/MainApp.java
@@ -4,23 +4,15 @@ import com.library.service.BookService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Main application entry point.
- * Loads the Spring application context from applicationContext.xml
- * and demonstrates retrieval/usage of the configured beans.
- */
 public class MainApp {
 
     public static void main(String[] args) {
 
-        // Load the Spring container using the XML configuration file
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        // Retrieve the BookService bean from the context
         BookService bookService = (BookService) context.getBean("bookService");
 
-        // Test the configuration
         bookService.addBook("Effective Java");
         bookService.getBook(101);
 
