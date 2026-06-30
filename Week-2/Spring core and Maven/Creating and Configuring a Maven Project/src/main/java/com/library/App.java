4ae079ec2d8b9952a2feecc5a6d4bfd08c45957b
R Bhuvan Dhanush <bhuvandhanush2006@gmail.com>
2026-06-30 09:21:41 +0530

    Add files via upload

---
+++ Week-2/Spring core and Maven/Creating and Configuring a Maven Project/src/main/java/com/library/App.java
@@ -0,0 +1,9 @@
+package com.library;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("LibraryManagement Maven project is set up successfully!");
+        System.out.println("Spring Context, Spring AOP, and Spring WebMVC dependencies are ready to use.");
+    }
+}
