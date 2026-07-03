67dc1a25130f0ebf6a0b500ab3695272debe9b41
R Bhuvan Dhanush <bhuvandhanush2006@gmail.com>
2026-07-03 14:06:18 +0530

    Add files via upload

---
+++ Week-4/Microservices with Spring Boot 3 and Spring Cloud/Creating Microservices for account and loan/loan/src/main/java/com/cognizant/loan/controller/LoanController.java
@@ -0,0 +1,19 @@
+package com.cognizant.loan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoanController {
+
+	@GetMapping("/loans/{number}")
+	public Loan getLoanDetails(@PathVariable String number) {
+
+		// Dummy response - no backend connectivity
+		Loan loan = new Loan(number, "car", 400000, 3258, 18);
+
+		return loan;
+	}
+
+}
